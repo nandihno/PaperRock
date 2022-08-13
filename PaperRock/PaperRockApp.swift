973559d546779cb68f635360fd15a5e534d4330c
Nando <fernando.deleon@gmail.com>
2022-08-14 05:12:41 +1000

    Initial Commit

---
+++ PaperRock/PaperRockApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaperRockApp.swift
+//  PaperRock
+//
+//  Created by Fernando De Leon on 14/8/2022.
+//
+
+import SwiftUI
+
+@main
+struct PaperRockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
